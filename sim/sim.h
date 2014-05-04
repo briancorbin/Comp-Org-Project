7fb27290496655d2f7dd7f6a45c6112d1d156801
Brian Corbin <brian.william.corbin@gmail.com>
2014-05-04 15:33:55 -0400

    All OP Codes added

--- sim/sim.h
+++ sim/sim.h
@@ -21,31 +21,54 @@
 enum opcodes
 {
 	OP_RTYPE	= 0x00,
+    OP_BGEZ     = 0x01, //rt = 00001
+    OP_BGEZAL   = 0x01, //rt = 10001
+    OP_BLTZ     = 0x01, //rt = 00000
+    OP_BLTZAL   = 0x01, //rt = 10000
     OP_J        = 0x02,
-	OP_JAL		= 0x03,
+    OP_JAL      = 0x03,
     OP_BEQ      = 0x04,
     OP_BNE      = 0x05,
+    OP_BLEZ     = 0x06,
+    OP_BGTZ     = 0x07,
     OP_ADDI     = 0x08,
-	OP_ADDIU	= 0x09,
+    OP_ADDIU    = 0x09,
     OP_SLTI     = 0x0a,
     OP_SLTIU    = 0x0b,
     OP_ANDI     = 0x0c,
     OP_ORI      = 0x0d,
-	OP_LUI		= 0x0f,
-	OP_LW		= 0x23,
-    OP_LBU      = 0x24,
-    OP_LHU      = 0x25,
+    OP_XORI     = 0x0e,
+    OP_LUI      = 0x0f,
+    OP_LB       = 0x20,
+    OP_LW       = 0x23,
     OP_SB       = 0x28,
-    OP_SH       = 0x29,
 	OP_SW		= 0x2b
 };
 
 enum functions
 {
+    FUNC_SLL        = 0x00,
+    FUNC_SRL        = 0x02,
+    FUNC_SRA        = 0x03,
+    FUNC_SLLV       = 0x04,
+    FUNC_SRLV       = 0x05,
 	FUNC_JR			= 0x08,
 	FUNC_SYSCALL	= 0x0c,
+    FUNC_MFHI       = 0x10,
+    FUNC_MFLO       = 0x11,
+    FUNC_MULT       = 0x18,
+    FUNC_MULTU      = 0x19,
+    FUNC_DIV        = 0x1a,
+    FUNC_DIVU       = 0x1b,
+    FUNC_ADD        = 0x20,
 	FUNC_ADDU		= 0x21,
-	FUNC_OR			= 0x25
+    FUNC_SUB        = 0x22,
+    FUNC_SUBU       = 0x23,
+    FUNC_AND        = 0x24,
+	FUNC_OR			= 0x25,
+    FUNC_XOR        = 0x26,
+    FUNC_SLT        = 0x2a,
+    FUNC_SLTU       = 0x2b
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
